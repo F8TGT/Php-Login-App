0ab4e1d00fb63068fd4e3e6142ab17e456e3fb21
Edward <78192184+F8TGT@users.noreply.github.com>
2025-04-14 08:25:15 -0600

    → Added functionality to the login.

--- login.php
+++ login.php
@@ -13,8 +13,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (mysqli_num_rows($result) === 1) {
         $user = mysqli_fetch_assoc($result);
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['logged_in'] = true;
+            $_SESSION['username'] = $user['username'];
+            header('Location: admin.php');
+            exit;
+        } else {
+            $error = "Invalid username or password";
+        }
     } else {
-
+        $error = "User not found";
     }
 }
 ?>
@@ -26,10 +34,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
+    <title>Login</title>
 </head>
 <body>
-<h2>Register</h2>
+<h2>Login</h2>
 <?php
 if ($error): ?>
     <p style="color:red">
@@ -46,7 +54,7 @@ endif; ?>
     <label for="password">Password:</label>
     <input id="password" type="password" name="password" required>
 
-    <input type="submit" value="Register">
+    <input type="submit" value="Login">
 </form>
 </body>
 </html>
