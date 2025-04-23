b70e2f6fb1878b969fcf37f8865734fb439e5885
Edward <78192184+F8TGT@users.noreply.github.com>
2025-04-23 08:44:26 -0600

    → Added indication of the active page in the navigation.

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,23 +1,44 @@
+<?php
+
+$current_page = basename($_SERVER['PHP_SELF']);
+echo $current_page;
+?>
+
 <nav>
     <ul>
         <li>
-            <a href="index.php">Home</a>
+            <a class="<?php
+            if ($current_page === "index.php") {
+                echo "active";
+            } ?>" href="index.php">Home</a>
         </li>
         <?php
         if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
             <li>
-                <a href="admin.php">Admin</a>
+                <a class="<?php
+                if ($current_page === "admin.php") {
+                    echo "active";
+                } ?>" href="admin.php">Admin</a>
             </li>
             <li>
-                <a href="logout.php">Logout</a>
+                <a class="<?php
+                if ($current_page === "logout.php") {
+                    echo "active";
+                } ?>" href="logout.php">Logout</a>
             </li>
         <?php
         else: ?>
             <li>
-                <a href="register.php">Register</a>
+                <a class="<?php
+                if ($current_page === "register.php") {
+                    echo "active";
+                } ?>" href="register.php">Register</a>
             </li>
             <li>
-                <a href="login.php">Login</a>
+                <a class="<?php
+                if ($current_page === "login.php") {
+                    echo "active";
+                } ?>" href="login.php">Login</a>
             </li>
         <?php
         endif; ?>
