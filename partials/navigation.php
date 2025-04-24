1bc1081cdf675fa000cf7ec87f121af80ab48987
Edward <78192184+F8TGT@users.noreply.github.com>
2025-04-24 11:16:36 -0600

    → Added indication of the active page in the navigation.

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,44 +1,33 @@
 <?php
 
-$current_page = basename($_SERVER['PHP_SELF']);
-echo $current_page;
+include "functions.php";
 ?>
 
 <nav>
     <ul>
         <li>
             <a class="<?php
-            if ($current_page === "index.php") {
-                echo "active";
-            } ?>" href="index.php">Home</a>
+            echo setActiveClass('index.php') ?>" href="index.php">Home</a>
         </li>
         <?php
         if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
             <li>
                 <a class="<?php
-                if ($current_page === "admin.php") {
-                    echo "active";
-                } ?>" href="admin.php">Admin</a>
+                echo setActiveClass('admin.php') ?>" href="admin.php">Admin</a>
             </li>
             <li>
                 <a class="<?php
-                if ($current_page === "logout.php") {
-                    echo "active";
-                } ?>" href="logout.php">Logout</a>
+                echo setActiveClass('logout.php') ?>" href="logout.php">Logout</a>
             </li>
         <?php
         else: ?>
             <li>
                 <a class="<?php
-                if ($current_page === "register.php") {
-                    echo "active";
-                } ?>" href="register.php">Register</a>
+                echo setActiveClass('register.php') ?>" href="register.php">Register</a>
             </li>
             <li>
                 <a class="<?php
-                if ($current_page === "login.php") {
-                    echo "active";
-                } ?>" href="login.php">Login</a>
+                echo setActiveClass('login.php') ?>" href="login.php">Login</a>
             </li>
         <?php
         endif; ?>
